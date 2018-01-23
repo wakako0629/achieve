5c6b7fe86f543cc82c7f77537d0ae814212a7f32
Wakako_Aoki <w-aoki@nttpc.co.jp>
2018-01-23 00:09:13 +0000

    init

--- app/mailers/blog_mailer.rb
+++ app/mailers/blog_mailer.rb
@@ -1,7 +1,8 @@
 class BlogMailer < ApplicationMailer
+    
     def blog_mail(blog)
-    @blog = blog
-
-    mail to: @blog.user.email, subject: "ブログ作成完了！"
+      @blog = blog
+      mail to: @blog.user.email, subject: "ブログ作成完了！"
     end
+    
 end
